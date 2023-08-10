17f207b1f3c9edd48b7d38bc0498700c1172ccda
Lee-nkoana <lebonkoana6@gmail.com>
2023-08-10 22:37:06 +0200

    Made changes to logic of how each round is run

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//aarray  of the available options the computer and user can pick from
 const options = ["rock", "paper", "scissors"];
 let userScore = 0;
 let computerScore = 0;
@@ -6,54 +7,57 @@ console.log(options);
 console.log("user score is " + userScore);
 console.log("computer score is " + computerScore);
 
+//Get a random option for the computer
 function getComputerChoice(){
     return options[(Math.floor(Math.random() * options.length))];    
 }
 
-let computerPick = getComputerChoice()
-console.log(computerPick);
+//Setting the computers pick to be the out put of the getComputerChoice function
+let computerChoice = getComputerChoice()
+console.log(computerChoice);
 
-function playRound(){
-    let playerPick = prompt('pick between rock, paper, and scissors');
-
-    function getUserChoice(playerPick){
-        return playerPick;
-    }
-
-    console.log(playerPick);
-    getUserChoice();
-
-    if(playerPick === computerPick){
+//function for each single round of the game
+function playRound(playerPick, computerPick){
+    if(usersChoice.toLowerCase() === computerChoice){
         console.log("it is a tie");
     }
-    else if((playerPick === "rock") && (computerPick === "paper")){
+    else if((usersChoice.toLowerCase() === "rock") && (computerChoice === "paper")){
         computerScore++
         console.log("computer gets a point");
     }
-    else if((playerPick === "rock") && (computerPick === "scissors")){
+    else if((usersChoice.toLowerCase() === "rock") && (computerChoice === "scissors")){
         userScore++
         console.log("player gets a point");
     }
-    else if((playerPick === "paper") && (computerPick === "scissors")){
+    else if((usersChoice.toLowerCase() === "paper") && (computerChoice === "scissors")){
         computerPick++
         console.log("computer gets a point");
     }
-    else if((playerPick === "paper") && (computerPick === "rock")){
+    else if((usersChoice.toLowerCase() === "paper") && (computerChoice === "rock")){
         userScore++
         console.log("user gets a point");
     }
-    else if((playerPick === "scissors") && (computerPick === "rock")){
+    else if((usersChoice.toLowerCase() === "scissors") && (computerChoice === "rock")){
         computerScore++
-        console.log("computer gets a point")
+        console.log("computer gets a point");
     }
-    else if((playerPick === "scissors") && (computerPick === "paper")){
+    else if((usersChoice.toLowerCase() === "scissors") && (computerChoice === "paper")){
         userScore++
-        console.log("user gets a point")
+        console.log("user gets a point");
     }
+    else{
+        alert("Invalid option");
+    }
+
+    console.log("user: " + userScore);
+    console.log("computer: " + computerScore);
+
+    return(console.log(usersChoice, computerChoice));
 }
+
+//prompt the user to enter their pick
+let usersChoice = prompt('pick between rock, paper, and scissors');
 
 playRound();
 
-console.log("user score is " + userScore);
-console.log("computer score is " + computerScore);
 
